553dc2ca49f5e1259c5ded82887f812db9d1620c
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-28 04:21:02 +0100

    rename to AppMain and fix indentations

---
+++ FlashcardsAR/AppMain.swift
@@ -0,0 +1,17 @@
+//
+//  AppMain.swift
+//  FlashcardsAR
+//
+//  Created by Zaid Neurothrone on 2022-12-28.
+//
+
+import SwiftUI
+
+@main
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
